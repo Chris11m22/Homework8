ebb879bfebd99894c87e9c1c6308f784f9ebc58a
christinam <malininak11@gmail.com>
2022-10-11 00:10:58 +0500

    дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] sides = new int[3];
+        sides[0] = 1 + 1;
+        sides[1] = 2;
+        sides[2] = 3 + 1;
+        System.out.print(sides[0] + ", ");
+        System.out.print(sides[1] + ", ");
+        System.out.print(sides[2] + ", ");
+    }
+    {
+
+        double[] as = {1.57, 7.654, 9.986};
+        for (int i = 0; i <as.length; i++) {
+            System.out.print(as[1]);
+            if (i != as.length - 1) {
+              System.out.print(", ");
+            }
+        }
+
     }
 }
+
+
+
+
